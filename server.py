b1f6bfdb0e60b0eeecb6cb3a6b2e8d03604a11c1
vagrant <vagrant@precise32.(none)>
2014-09-20 14:35:39 +0000

    Adding initial flask files

---
+++ server.py
@@ -0,0 +1,12 @@
+"""
+Flask dev server module
+"""
+import simplixity
+
+
+if __name__ == '__main__':
+    print 'Simplixity Server'
+    simplixity.application.run()
+
+
+
---
+++ simplixity/app.py
@@ -0,0 +1,9 @@
+"""
+app.py
+
+Main Simplixity Flask Application
+"""
+
+from flask import Flask
+application = Flask(__name__)
+
